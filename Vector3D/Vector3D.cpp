8ced776c348bc92b5515f5422d8b9f386dbc33ec
pierre-teodoresco <p.teodoresco@gmail.com>
2024-09-16 09:04:02 -0400

    first commit

---
+++ Vector3D/Vector3D.cpp
@@ -0,0 +1,112 @@
+//
+//  Vector3D.cpp
+//  MoteurPhysique
+//
+//  Created by Pierre Teodoresco on 16/09/2024.
+//
+
+#include "Vector3D.hpp"
+#include <cmath>
+
+/* CONSTRUCTORS */
+Vector3D::Vector3D(float x, float y, float z): m_x(x), m_y(y), m_z(z) {}
+
+/* DESTRUCTOR */
+Vector3D::~Vector3D() {}
+
+/* GETTERS */
+
+// Attributes
+inline float Vector3D::x() const { return m_x; }
+inline float Vector3D::y() const { return m_y; }
+inline float Vector3D::z() const { return m_z; }
+
+// Computed values
+float Vector3D::norm() const {
+    return sqrt(squaredNorm());
+}
+
+inline float Vector3D::squaredNorm() const {
+    return m_x*m_x + m_y*m_y + m_z*m_z;
+}
+
+/* OPERATIONS */
+
+// Scalar multiplication
+Vector3D Vector3D::operator*(float scalar) const {
+    // RVO (return value optimization) to avoid useless Constructor call
+    return {m_x*scalar, m_y*scalar, m_z*scalar};
+}
+
+Vector3D& Vector3D::operator*=(float scalar) {
+    m_x *= scalar;
+    m_y *= scalar;
+    m_z *= scalar;
+    return *this;
+}
+
+// Cross product (produit vectoriel)
+Vector3D Vector3D::operator*(const Vector3D& other) const {
+    return {
+        m_y * other.z() - m_z * other.y(),
+        m_z * other.x() - m_x * other.z(),
+        m_x * other.y() - m_y * other.x()
+    };
+}
+
+Vector3D& Vector3D::operator*=(const Vector3D& other) {
+    m_x = m_y * other.z() - m_z * other.y();
+    m_y = m_z * other.x() - m_x * other.z();
+    m_z = m_x * other.y() - m_y * other.x();
+    return *this;
+}
+
+// Sum
+Vector3D Vector3D::operator+(const Vector3D& other) const {
+    return {m_x + other.x(), m_y + other.y(), m_z + other.z()};
+}
+
+Vector3D& Vector3D::operator+=(const Vector3D& other) {
+    m_x += other.x();
+    m_y += other.y();
+    m_z += other.z();
+    return *this;
+}
+
+// Subtraction
+Vector3D Vector3D::operator-(const Vector3D& other) const {
+    return {m_x - other.x(), m_y - other.y(), m_z - other.z()};
+}
+
+Vector3D& Vector3D::operator-=(const Vector3D& other) {
+    m_x -= other.x();
+    m_y -= other.y();
+    m_z -= other.z();
+    return *this;
+}
+
+// Dot product (produit scalaire)
+float Vector3D::dot(const Vector3D& other) const {
+    return m_x * other.x() + m_y * other.y() + m_z * other.z();
+}
+
+// Components product
+Vector3D Vector3D::componentsProduct(const Vector3D& other) const {
+    return {m_x * other.x(), m_y * other.y(), m_z * other.z()};
+}
+
+// Normalization
+Vector3D Vector3D::normalize() const {
+    float normValue = norm();
+    if (normValue == 0) return Vector3D();
+    return {m_x / normValue, m_y / normValue, m_z / normValue};
+}
+
+void Vector3D::normalize() {
+    float normValue = norm();
+    if (normValue != 0) {
+        m_x /= normValue;
+        m_y /= normValue;
+        m_z /= normValue;
+    }
+}
---
+++ Vector3D/Vector3D.hpp
@@ -0,0 +1,63 @@
+//
+//  Vector3D.hpp
+//  MoteurPhysique
+//
+//  Created by Pierre Teodoresco on 16/09/2024.
+//
+
+#ifndef Vector3D_hpp
+#define Vector3D_hpp
+
+class Vector3D {
+
+private:
+    float m_x, m_y ,m_z;
+    
+public:
+    /* CONSTRUCTORS */
+    Vector3D(float x = 0.f, float y = 0.f, float z = 0.f);
+    
+    /* DESTRUCTOR */
+    ~Vector3D();
+    
+    /* GETTERS */
+    
+    // Attributes
+    float x() const;
+    float y() const;
+    float z() const;
+    
+    // Computed values
+    float norm() const;
+    float squaredNorm() const;
+    
+    /* OPERATIONS */
+    
+    // Scalar multiplication
+    Vector3D operator*(float scalar) const;
+    Vector3D& operator*=(float other);
+    
+    // Cross product (produit vectoriel)
+    Vector3D operator*(const Vector3D& other) const;
+    Vector3D& operator*=(const Vector3D& other);
+    
+    // Sum
+    Vector3D operator+(const Vector3D& other) const;
+    Vector3D& operator+=(const Vector3D& other);
+    
+    // Subtraction
+    Vector3D operator-(const Vector3D& other) const;
+    Vector3D& operator-=(const Vector3D& other);
+    
+    // Dot product (produit scalaire)
+    float dot(const Vector3D& other) const;
+    
+    // Components product
+    Vector3D componentsProduct(const Vector3D& other) const;
+    
+    // Normalization
+    Vector3D normalize() const;
+    void normalize();
+};
+
+#endif /* Vector3D_hpp */
